e2bdd9dab41d4acde4222ba43312cf8ad33dc8ee
xzhih <triton2you@gmail.com>
2021-05-17 03:24:24 +0800

    init app

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracesSpacing: true,
+  trailingComma: 'none'
+};
